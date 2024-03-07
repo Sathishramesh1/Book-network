3c2400a487bcbff90f951c75718a62bf98d29159
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-03-08 00:03:38 +0530

    user-model-error

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { User } from './model/UserModel';
+import { User } from './model/UserModel.js';
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
