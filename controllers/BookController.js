169f88889b0d93f862495c78a9af0065c11471e7
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-03-08 16:24:39 +0530

    render-error

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -126,7 +126,7 @@ export {favbook}
 
 const getfav=async(req,res)=>{
     try {
-        const { email } = req.query;
+        const  email  = req.query.email;
 
 // Find the user by their email
 console.log(email);
